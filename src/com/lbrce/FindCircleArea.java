455e2f4ecd5cd9e4dbf96d25a62d1b9addbe5529
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-11-08 09:52:50 +0530

    #team leader 5415 Committed locally

---
+++ src/com/lbrce/FindCircleArea.java
@@ -0,0 +1,23 @@
+package com.lbrce;
+
+public class FindCircleArea {
+	public static double findArea(double radius)
+	{
+		
+	}
+
+}
+
+
+
+
+package com.lbrce;
+
+
+
+public static double findArea(double length, double width)
+{
+	
+}
+
+public static double findSurfaceArea(double length, double width, double height)
---
+++ src/com/lbrce/FindRectanlgeArea.java
@@ -0,0 +1,9 @@
+package com.lbrce;
+
+public class FindRectanlgeArea {
+	public static double findArea(double length, double width)
+	{
+		
+	}
+
+}
