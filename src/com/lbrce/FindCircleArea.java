5baa0cdb6381a492faba32f7f5f2f0081cfe892e
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-11-08 09:53:45 +0530

    #committed

--- src/com/lbrce/FindCircleArea.java
+++ src/com/lbrce/FindCircleArea.java
@@ -11,13 +11,9 @@ public class FindCircleArea {
 
 
 
-package com.lbrce;
 
 
 
-public static double findArea(double length, double width)
-{
-	
-}
 
-public static double findSurfaceArea(double length, double width, double height)
+
+
--- src/com/lbrce/FindCuboidArea.java
+++ src/com/lbrce/FindCuboidArea.java
@@ -1,5 +1,9 @@
 package com.lbrce;
 
 public class FindCuboidArea {
+	public static double findSurfaceArea(double length, double width, double height)
+	{
+		
+	}
 
 }
