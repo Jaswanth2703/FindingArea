1f477c830a50fa3d8e4c89d2fe3a19d281631a43
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-11-08 10:45:20 +0530

    I did area of cuboid!!

--- src/com/lbrce/FindCuboidArea.java
+++ src/com/lbrce/FindCuboidArea.java
@@ -3,7 +3,8 @@ package com.lbrce;
 public class FindCuboidArea {
 	public static double findSurfaceArea(double length, double width, double height)
 	{
-		
+		 double surfaceArea = 2 * ((length * width) + (length * height) + (width * height));
+	        return surfaceArea;
 	}
 
 }
